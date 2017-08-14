9831b69aaf86ce6232c4b4cbef173b75bd72bec0
Akanksha <akanksha.sharma@monjin.com>
2017-08-14 14:30:05 +0530

    added websocket connection updating table on recieving data css changes

--- src/components/stock-table/stock-table.js
+++ src/components/stock-table/stock-table.js
@@ -1,8 +1,66 @@
+const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', 'hour': 'numeric', 'minute': 'numeric', 'second': 'numeric' }
 export default {
   name: 'hello',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      tableData: {},
+      tableArray: []
+    }
+  },
+  methods: {
+    handleDataChange (data) {
+      let parsedData = JSON.parse(data)
+      let newTableObj = this.convertArrayToObj(parsedData)
+      this.tableData = this.generateTableData(newTableObj)
+    },
+    convertArrayToObj (array) {
+      let tableEntries = {}
+      array.forEach(function (item) {
+        Object.assign(tableEntries, {
+          [ item[0] ]: item[1]
+        })
+      })
+      return tableEntries
+    },
+    generateTableData (newTableObj) {
+      let tableData = Object.assign({}, this.tableData)
+      for (let key in newTableObj) {
+        if (tableData[key]) {
+          if (this.tableData[key].oldValue) {
+            tableData[key] = {
+              oldValue: [...this.tableData[key].oldValue, this.tableData[key].newValue],
+              newValue: this.roundOffPrice(newTableObj[key]),
+              lastUpdated: new Date().toLocaleString(undefined, options)
+            }
+          } else {
+            tableData[key] = {
+              oldValue: [this.tableData[key].newValue],
+              newValue: this.roundOffPrice(newTableObj[key]),
+              lastUpdated: new Date().toLocaleString(undefined, options)
+            }
+          }
+        } else {
+          tableData[key] = {
+            newValue: this.roundOffPrice(newTableObj[key]),
+            lastUpdated: new Date().toLocaleString(undefined, options)
+          }
+        }
+      }
+      return tableData
+    },
+    roundOffPrice: price => Math.round(price * 100) / 100
+  },
+  created () {
+    var self = this
+    let socket = new WebSocket('ws://stocks.mnet.website')
+
+   // Show a connected message when the WebSocket is opened.
+    socket.onopen = function (event) {
+      console.log('Connected to: ' + event.currentTarget.URL)
+    }
+    socket.onmessage = function (event) {
+      self.handleDataChange(event.data)
     }
   }
 }
--- src/components/stock-table/stock-table.vue
+++ src/components/stock-table/stock-table.vue
@@ -1,23 +1,41 @@
 <template>
-<md-table v-once>
+<md-table>
   <md-table-header>
     <md-table-row>
-      <md-table-head>Dessert (100g serving)</md-table-head>
-      <md-table-head md-numeric>Calories (g)</md-table-head>
-      <md-table-head md-numeric>Fat (g)</md-table-head>
-      <md-table-head md-numeric>Carbs (g)</md-table-head>
-      <md-table-head md-numeric>Protein (g)</md-table-head>
+      <md-table-head class="header">Ticker</md-table-head>
+      <md-table-head class="header">Price</md-table-head>
+      <md-table-head class="header">Time</md-table-head>
     </md-table-row>
   </md-table-header>
 
   <md-table-body>
-    <md-table-row v-for="(row, index) in 5" :key="index">
-      <md-table-cell>Dessert Name</md-table-cell>
-      <md-table-cell v-for="(col, index) in 4" :key="index" md-numeric>10</md-table-cell>
+    <md-table-row v-for="(row, index) in tableData"  :key="index">
+    <md-table-cell>{{index}}</md-table-cell>
+      <md-table-cell v-if="!row.oldValue" style="background-color:#fff" >{{row.newValue}}</md-table-cell>
+      <md-table-cell v-if="row.oldValue && row.newValue>=row.oldValue[row.oldValue.length-1]" style="background-color:#4CAF50">{{row.newValue}}</md-table-cell>
+      <md-table-cell v-if="row.oldValue && row.newValue<row.oldValue[row.oldValue.length-1]" style="background-color:#F44336">{{row.newValue}}</md-table-cell>
+      <md-table-cell>{{row.lastUpdated}}</md-table-cell>
     </md-table-row>
   </md-table-body>
 </md-table>
 </template>
 
 <script src='./stock-table.js'></script>
+
+<style>
+.md-table .md-table-head.header{
+  text-align: center;
+    font-size: 1rem;
+    text-transform: uppercase;
+    color: #000000;
+}
+.md-table .md-table-cell.md-has-action .md-table-cell-container{
+  justify-content:space-around;
+  
+}
+.md-table-cell-container{
+font-weight:500;
+}
+</style>
+
 
